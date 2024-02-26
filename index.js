489d733a8e2b44a11d42d80bd60c6e6e863ea82e
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-02-27 00:30:17 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+var customerName;
+customerName = `bob`;
+console.log(customerName);
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    return (customerName = customerName.toUpperCase());
+  } else undefined;
+}
+customerName = upperCaseCustomerName(customerName);
+
+console.log(upperCaseCustomerName(customerName));
+
+// second part
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+setBestCustomer();
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "someone else";
+}
